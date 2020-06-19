a962b3dfa81a23c71a33e77fc7624e14203b51d5
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-19 13:59:43 -0300

    feat: update task status

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
 
 @Controller('tasks')
@@ -20,6 +20,14 @@ export class TasksController {
   @Post()
   create(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.create(createTaskDto)
+  }
+
+  @Patch('/:id/status')
+  updateStatus(
+    @Param('id') id: string, 
+    @Body('status') status: TaskStatus
+  ): Task {
+    return this.tasksService.updateStatus(id, status);
   }
 
   @Delete('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -29,6 +29,12 @@ export class TasksService {
     return task;
   }
 
+  updateStatus(id: string, status: TaskStatus): Task {
+    const task = this.getById(id);
+    task.status = status;
+    return task;
+  }
+
   delete(id: string): void {
     this.tasks = this.tasks.filter(task => task.id !== id);
   }
