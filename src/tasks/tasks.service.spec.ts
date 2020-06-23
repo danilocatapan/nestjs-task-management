36deef10598ee0eacfc643c1d254b9b89c6a7029
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-23 14:44:11 -0300

    test: ensure gets task by id

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -4,18 +4,21 @@ import { TaskRepository } from './task.repository';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './task-status.enum';
 import * as faker from 'faker';
+import { NotFoundException } from '@nestjs/common';
 
-const mockUser = { 
+const mockUser = {
+  id: faker.random.number,
   username: faker.fake.name
 };
 
-const mockTasks = {
+const mockTask = {
   title: faker.lorem.word(),
   description: faker.lorem.words()
 }
 
 const mockTaskRepository = () => ({
-  getTasks: jest.fn()
+  getTasks: jest.fn(),
+  findOne: jest.fn()
 });
 
 describe('TasksService', () => {
@@ -38,12 +41,35 @@ describe('TasksService', () => {
 
   describe('getTasks', () => {
     test('gets all tasks from the repository', async () => {
-      taskRepository.getTasks.mockResolvedValue(mockTasks);
+      taskRepository.getTasks.mockResolvedValue(mockTask);
       expect(taskRepository.getTasks).not.toHaveBeenCalled();
       const filters: GetTasksFilterDto = { status: TaskStatus.IN_PROGRESS, search: faker.lorem.word() };
       const result = await tasksService.getTasks(filters, mockUser);
       expect(taskRepository.getTasks).toHaveBeenCalled();
-      expect(result).toEqual(mockTasks);
-    })
+      expect(result).toEqual(mockTask);
+    });
   })
+
+  describe('getTaskById', () => {
+    test('calls taskRepository.findOne() and successfully retrieve and return the task', async () => {
+      taskRepository.findOne.mockResolvedValue(mockTask);
+
+      const id = faker.random.number;
+      const result = await tasksService.getTaskById(id, mockUser);
+      expect(result).toEqual(mockTask);
+
+      expect(taskRepository.findOne).toHaveBeenCalledWith({ 
+        where: { 
+          id, 
+          userId: mockUser.id
+        }
+      })
+    });
+
+    test('throws an error as task is not found', () => {
+      taskRepository.findOne.mockResolvedValue(null);
+      expect(tasksService.getTaskById(1, mockUser)).rejects.toThrow(NotFoundException);
+    });
+  })
+  
 })
